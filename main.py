f3307c3743b3e248da7dfae15b9f230e57a549e9
Jonathan <3190208@mail.zih.tu-dresden.de>
2023-08-28 15:13:45 +0200

    Switch results format from json to csv

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import json
+import pandas as pd
 from query import query_endpoint
 
 
@@ -27,16 +29,38 @@ def load_sparql_query(query_path):
         return file.read()
 
 
+def create_results_json(data, filename="dbversions.json"):
+    save_json = os.path.join(basepath, filename)
+    save_csv = os.path.join(basepath, "dbversions2.csv")
+    # output = json.dumps(data, indent=4)
+    # with open(result_json, "w") as file:
+    #     file.write(output)
+
+    json_obj = json.dumps(data, indent=4)
+    print("pree")
+
+    with open(save_json, "w") as output:
+        output.write(json_obj)
+        df = pd.read_json(json_obj)
+        df.to_csv(save_csv, sep=',', encoding='utf-8', index=False)
+
+
 endpoints = get_endpoints(endpoints_path)
 query = load_sparql_query(stats_query_path)
 
 
 def main():
+    endpoint_results = {}
+
     for endpoint in endpoints:
-        db = query_endpoint(endpoint, query)
-        version_count = db["Version"]["value"]
-        endpoint_versions[endpoint] = version_count
-        endpoint_versions["totalVersions"] = f"{int(version_count) + int(endpoint_versions['totalVersions'])}" 
+        db = query_endpoint(endpoint, query, stats_query_path)
+        endpoint_results[endpoint] = db
+        print(db)
+        # version_count = db["results"]["bindings"][0]["Version"]["value"]
+        # endpoint_versions[endpoint] = version_count
+        # endpoint_versions["totalVersions"] = f"{int(version_count) + int(endpoint_versions['totalVersions'])}" 
+    
+    # create_results_json(endpoint_results)
     return endpoint_versions
 
 
--- query.py
+++ query.py
@@ -1,19 +1,61 @@
-from SPARQLWrapper import SPARQLWrapper, JSON
+import os
+import re
+import csv
+import json
+from textwrap import fill
+from SPARQLWrapper import SPARQLWrapper, JSON, CSV
 
 
-def query_endpoint(endpoint, query):
+def save_result(result, filename="dbversion.json", fcsv="dbversion.csv"):
+    basepath = os.getcwd()
+    save_csv = os.path.join(basepath, fcsv)
+    save_json = os.path.join(basepath, filename)
+    json_obj = json.dumps(result, indent=4)
+
+
+
+    # with open(save_json, "w") as output:
+    #     output.write(json_obj)
+
+    # with open(save_csv, "w") as file:
+    #     csv_writer = csv.writer(file)
+    #     for row in result:
+    #         print(row)
+    return result
+
+
+def query_endpoint(endpoint, query, query_path):
     sparql = SPARQLWrapper(
         endpoint
     )
 
-    sparql.setReturnFormat(JSON)
+    # sparql.setReturnFormat(JSON)
+    sparql.setReturnFormat(CSV)
 
     try:
         sparql.setQuery(query)
         response = sparql.queryAndConvert()
+        data = response.decode('utf-8').splitlines()
 
-        for result in response["results"]["bindings"]:
-            return result
+        basepath = os.getcwd()
+        name = re.sub("http[s]://", "", endpoint)
+        name = re.sub("\/", "-", name)
+        filepath = os.path.join(basepath, f"{query_path}-{name}.csv")
+
+        with open(filepath, 'w+') as csv_file:
+            writer = csv.writer(csv_file, delimiter='\n')
+            for line in data:
+                writer.writerow(re.split('\n', line))
+
+
+        # print(response)
+        # response = save_result(response)
+        # print(response)
+        # print(response)
+        return response
+
+        # for result in response["results"]["bindings"]:
+        #     return result
 
     except Exception as error:
         print(error)
